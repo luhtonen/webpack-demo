563bb583650c43228a6518df071254fa4c2e6524
luhtonen <luhtonen@gmail.com>
2016-05-11 08:51:41 +0200

    Splitting webpack configuration with webpack-merge

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const merge = require('webpack-merge');
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: {
     app: PATHS.app
   },
@@ -20,3 +22,16 @@ module.exports = {
     })
   ]
 };
+
+let config;
+
+// detect how npm is run and branch based on that
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(common, {});
+}
+
+module.exports = config;
