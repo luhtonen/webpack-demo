bce3e7cd07ce65c2c816160ca474779b6ed0ab13
luhtonen <luhtonen@gmail.com>
2016-05-11 10:01:45 +0200

    Adding configuration validation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const merge = require('webpack-merge');
+
+const validate = require('webpack-validator');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -34,4 +35,4 @@ switch (process.env.npm_lifecycle_event) {
     config = merge(common, {});
 }
 
-module.exports = config;
+module.exports = validate(config);
