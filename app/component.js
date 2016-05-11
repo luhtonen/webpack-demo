e3863669fd7f565d1ae674b43189328ecc1da278
luhtonen <luhtonen@gmail.com>
2016-05-11 08:42:01 +0200

    Survive.JS Webpack book - initial 'Hello world' setup

---
+++ app/component.js
@@ -0,0 +1,13 @@
+/**
+ * Created by luhtonen on 11/05/16.
+ */
+
+'use strict';
+
+module.exports = function () {
+  var element = document.createElement('h1');
+
+  element.innerHTML = 'Hello World';
+
+  return element;
+};
---
+++ app/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by luhtonen on 11/05/16.
+ */
+
+'use strict';
+
+var component = require('./component');
+
+document.body.appendChild(component());
