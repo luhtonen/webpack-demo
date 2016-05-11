9e84bcaea40e814e41017e767a0edcbf78d7f5bd
luhtonen <luhtonen@gmail.com>
2016-05-11 11:25:43 +0200

    Configuring hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
-
 const validate = require('webpack-validator');
+
+const parts = require('./lib/parts');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -32,7 +33,14 @@ switch (process.env.npm_lifecycle_event) {
     config = merge(common, {});
     break;
   default:
-    config = merge(common, {});
+    config = merge(
+      common,
+      parts.devServer({
+        // Customise host/port here if needed
+        host: process.env.HOST,
+        port: process.env.PORT
+      })
+    );
 }
 
 module.exports = validate(config);
