6840dee090a1cd91759e7a564a3b00d28dc7b612
luhtonen <luhtonen@gmail.com>
2016-05-11 16:48:10 +0200

    Eliminating unused CSS

--- app/component.js
+++ app/component.js
@@ -7,6 +7,7 @@
 module.exports = function () {
   var element = document.createElement('h1');
 
+  element.className = 'pure-button';
   element.innerHTML = 'Hello World';
 
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,10 @@ const pkg = require('./package.json');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  style: path.join(__dirname, 'app', 'main.css'),
+  style: [
+    path.join(__dirname, 'node_modules', 'purecss'),
+    path.join(__dirname, 'app', 'main.css')
+  ],
   build: path.join(__dirname, 'build')
 };
 
@@ -53,7 +56,8 @@ switch (process.env.npm_lifecycle_event) {
         entries: Object.keys(pkg.dependencies)
       }),
       parts.minify(),
-      parts.extractCSS(PATHS.style)
+      parts.extractCSS(PATHS.style),
+      parts.purifyCSS([PATHS.app])
     );
     break;
   default:
